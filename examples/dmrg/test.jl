653940d6b02bacdad19c658c96a816a0df3ec94b
nticea <nticea@stanford.edu>
2022-03-15 16:56:14 -0700

    hmm

--- examples/dmrg/test.jl
+++ examples/dmrg/test.jl
@@ -1,9 +1,9 @@
 using Pkg; 
-Pkg.activate("/Users/nicole/Dropbox/Grad school/Devereaux lab/ITensors.jl")
+Pkg.activate("/Users/nicole/Dropbox/Grad school/Devereaux lab/ITensors.jl/Project.toml")
 using ITensors
 # using SciPy: fft
 # using Plots
 # using NPZ
-# using JLD
+# using JLDå
 
 println("Hello, world!")
