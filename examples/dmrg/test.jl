f8bf399e89e0509d7d2b86a371325c179597a530
nticea <nticea@stanford.edu>
2022-03-15 16:59:23 -0700

    FINALLY

--- examples/dmrg/test.jl
+++ examples/dmrg/test.jl
@@ -1,9 +1,9 @@
 using Pkg; 
 Pkg.activate("/home/users/nticea/ITensors.jl/Project.toml")
 using ITensors
-# using SciPy: fft
-# using Plots
-# using NPZ
-# using JLD
+using SciPy: fft
+using Plots
+using NPZ
+using JLD
 
 println("Hello, world!")
