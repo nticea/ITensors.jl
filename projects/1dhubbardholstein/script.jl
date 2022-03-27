85195944e0fd15e4a03630c78e434f10c098e5be
nticea <nticea@stanford.edu>
2022-03-27 12:31:46 -0700

    ope

--- projects/1dhubbardholstein/script.jl
+++ projects/1dhubbardholstein/script.jl
@@ -32,7 +32,7 @@ TEBD_cutoff = 1E-10
 DMRG_cutoff = 1E-10
 
 # Saveout info 
-fname_out = Dates.format(now(), "no_doping_no_phonon_80")
+fname_out = Dates.format(now(), "0.05_doping_no_phonon_80")
 save_path = joinpath(@__DIR__,"outputs",fname_out*".h5")
 
 # Specify operators of interest
